af8a60771cbe56b6b3f4b4ae4c1e2ba66fd1d59c
Ben Clark <hello@clarkben.com>
2023-03-25 19:15:29 +0000

    Proper goto

--- src/routes/planets/+page.svelte
+++ src/routes/planets/+page.svelte
@@ -2,6 +2,7 @@
 <script>
     import {onMount} from 'svelte';
     import {planets, game} from "../store.js";
+    import {goto} from "$app/navigation";
 
     const completeStatusIcon = '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600"><path d="M7.7 404.6s115.2 129.7 138.2 182.7h99c41.5-126.7 202.7-429.1 341-535.1 28.5-36.8-43.4-52-101.4-27.6C397 61.3 232 341.8 201.2 409.2c-43.7 11.5-89.8-73.7-89.9-73.7z" fill="#6c4"/></svg>';
     const incompleteStatusIcon = '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" version="1.0" viewBox="0 0 100 100"><path d="m6.4 6.4 87.2 87.2" style="fill:none;fill-rule:evenodd;stroke:red;stroke-width:18.05195999;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/><path d="M6.4 93.6 93.8 6.4" style="fill:none;fill-rule:evenodd;stroke:red;stroke-width:17.80202103;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1"/></svg>';
@@ -37,6 +38,10 @@
     onMount(() => {
     });
 
+    function gotoPlanet() {
+        goto('/planet');
+    }
+
 </script>
 
 <h1>Planet Selection</h1>
@@ -46,7 +51,7 @@
     {#each $planets as planet}
         <li>
             <p class="planet-name">{planet.name}</p>
-            <a href="/planet"><img src="{getPlanetImg(planet.name)}" width="150px" height="150px" alt="planet"></a>
+            <a on:click="{gotoPlanet}"><img src="{getPlanetImg(planet.name)}" width="150px" height="150px" alt="planet"></a>
             <div class="status-container">
                 <span>Status</span>
                 {@html getStatusIcon(planet.name)}
